bd8185d1f6b58eecbdf87209ac2faacf03dda683
RgJeoung <ray971125@gmail.com>
2022-04-28 09:47:48 +0900

    Update Refining.py
    2022.04.28. LAB
    
    # PATCH NOTE
    -optimization
      -to_txt: add complete comment
      -to_sdf: fix complete comment
      -reply comments
    -to_png: minimized the code.
      (There was repeated code and minimized them.)

--- python/Refining.py
+++ python/Refining.py
@@ -11,14 +11,14 @@ from rdkit.Chem import Draw, PandasTools, MolFromSmiles
 class convert_format:
     def __init__(self, inputfile):
         self.inputfile = inputfile
-        if type(inputfile) == type(pd.DataFrame()):
+        if type(inputfile) == type(pd.DataFrame()): # input path and data when inputformat is dataframe.
             self.fm = 'dataframe'
             path = input('PATH:')
             name = input('NAME:')
             self.path = path
             self.name = name
         else:
-            lst = inputfile.split('/')
+            lst = inputfile.split('/') # else list the path, name and format.
             path = '/'.join(lst[:-1])
             name = lst[-1].split('.')[0]
             fm = lst[-1].split('.')[1]
@@ -58,6 +58,7 @@ class convert_format:
     def to_txt(self, sep='\t'):
         dataframe = self.to_dataframe()
         dataframe.to_csv(f'{self.path}/{self.name}.txt', sep=sep)
+        print(f'Your file "{self.name}" is successfully converted from {self.fm} to text file.')
 
     def to_xlsx(self):
         dataframe = self.to_dataframe()
@@ -66,73 +67,55 @@ class convert_format:
 
     def to_sdf(self):
         dataframe = self.to_dataframe()
-        ID = input(f'Your columns are {list(dataframe.columns)}. \n'
-                   'Which column do you want to select to ID?:')
+        ID = input(f'Your columns are {list(dataframe.columns)}. \n' # show your columns.
+                   'Which column do you want to select to ID?:')     # select ID column.
         PandasTools.AddMoleculeColumnToFrame(dataframe, 'Smiles', 'Molecule')
         auto = input('Do you want to set properties Automatically? [Y/N]:')
+        # if yes, input all of your propertiees except ID and Smiles.
         if auto.upper() == 'Y':
             columns = [x for x in dataframe.columns if not (x == ID or x == 'Smiles')]
+        # Or you can choose your properties.
         else:
             columns = input('Which columns do you want to use as properties?:').split(', ')
         PandasTools.WriteSDF(dataframe, f'{self.path}/{self.name}.sdf', molColName='Molecule',idName=str(ID), properties=columns)
-        print(f'Your file "{self.name}" is successfully converted from excel to sdf file.')
+        print(f'Your file "{self.name}" is successfully converted from {self.fm} to sdf file.')
 
     def to_png(self):
         dataframe = self.to_dataframe()
         ID = input(f'Your columns are {list(dataframe.columns)}. \n'
                    'Which column do you want to select as "ID"? [import column name]:\n'
                    'Or do you want to save only structure img? [import "only structure"]:')
+        # make new folder for images.
         try:
             os.mkdir(f"{self.path}/{self.name}_draw")
         except:
             print('Already exist folder')
+        # indicate structure name.
         if ID.upper() == 'ONLY STRUCTURE':
-            if len(list(dataframe['Smiles'])) <= 8:
-                if len(list(dataframe['Smiles'])) <= 4:
-                    mols = [MolFromSmiles(mol) for mol in list(dataframe['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=len(list(dataframe['Smiles'])),
-                                               subImgSize=(250, 250), legends=None)
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}.png")
-                else:
-                    mols = [MolFromSmiles(mol) for mol in list(dataframe['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=4,
-                                               subImgSize=(250, 250), legends=None)
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}.png")
-            else:
-                split = input(f'The file have {len(dataframe)} structures.\n'
-                              'How many structures do you want to include in one png file?:')
-                lst = np.array_split(dataframe, math.ceil(len(dataframe)/int(split)))
-                for idx, i in enumerate(lst):
-                    mols = [MolFromSmiles(mol) for mol in list(i['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=4,
-                                               subImgSize=(250, 250), legends=None)
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}_{idx}.png")
+            structure_name = None
         else:
-            if len(list(dataframe['Smiles'])) <= 8:
-                if len(list(dataframe['Smiles'])) <= 4:
-                    mols = [MolFromSmiles(mol) for mol in list(dataframe['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=len(list(dataframe['Smiles'])),
-                                               subImgSize=(250, 250), legends=list(dataframe[ID]))
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}.png")
-                else:
-                    mols = [MolFromSmiles(mol) for mol in list(dataframe['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=4,
-                                               subImgSize=(250, 250), legends=list(dataframe[ID]))
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}.png")
-            else:
-                split = input(f'The file have {len(dataframe)} structures.\n'
-                              'How many structures do you want to include in one png file?:')
-                lst = np.array_split(dataframe, math.ceil(len(dataframe)/int(split)))
-                for idx, i in enumerate(lst):
-                    mols = [MolFromSmiles(mol) for mol in list(i['Smiles'])]
-                    img = Draw.MolsToGridImage(mols, molsPerRow=4,
-                                               subImgSize=(250, 250), legends=list(i[ID]))
-                    img.save(f"{self.path}/{self.name}_draw/{self.name}_{idx+1}.png")
-        print(f'Your file "{self.name}" is successfully converted from {self.fm} to png file.')
+            structure_name = list(dataframe[ID])
+        # make splits if you want.
+        if len(list(dataframe['Smiles'])) >= 5:
+            split = input(f'The file have {len(dataframe)} structures.\n'
+                          'How many structures do you want to include in one png file?:')
+            lst = np.array_split(dataframe, math.ceil(len(dataframe) / int(split)))
+            for idx, i in enumerate(lst):
+                mols = [MolFromSmiles(mol) for mol in list(i['Smiles'])]
+                img = Draw.MolsToGridImage(mols, molsPerRow=4,
+                                           subImgSize=(250, 250), legends=structure_name)
+                img.save(f"{self.path}/{self.name}_draw/{self.name}_{idx}.png")
+        else:
+            mols = [MolFromSmiles(mol) for mol in list(dataframe['Smiles'])]
+            img = Draw.MolsToGridImage(mols, molsPerRow=len(list(dataframe['Smiles'])),
+                                       subImgSize=(250, 250), legends=structure_name)
+            img.save(f"{self.path}/{self.name}_draw/{self.name}.png")
 
     def abstract(self, start=0, finish=None):
         dataframe = self.to_dataframe()
+        # select rows which you indicated from input.
         dataframe = dataframe[start:finish]
+        # select columns you want.
         columns = input(f'Your columns are {list(dataframe.columns)}. \n'
                         'Which column do you want to abstract?:').split(', ')
         if len(columns) == 1:
